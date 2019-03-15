ea7cbf9ee7ebc73038f21918bc93a8d387705a2a
frajerzycki <frajerzycki@protonmail.com>
2019-03-15 21:29:38 +0100

    Second part of password added.

--- nspe.py
+++ nspe.py
@@ -1,4 +1,6 @@
 from sys import argv
+from random import randint
+import re
 
 
 def string_to_int_arr(str):
@@ -9,24 +11,28 @@ def sum_of_products(a, b):
     return sum(x*b[i] for i, x in enumerate(a))
 
 
-def encrypt(message, password):
-    m, p = string_to_int_arr(message), string_to_int_arr(password)
+def encrypt(m, p, w):
     k = len(p)
+    d = len(w)
     a = [p[i % k] - x for i, x in enumerate(m)]
     b, c = sum_of_products(a, a), sum_of_products(a, m) << 1
-    e = [(b*x - a[n]*c + p[n % k] % k) for n, x in enumerate(m)]
-    return [x % b for x in e], [x // b for x in e], b
+    e = [b*x - a[n]*c for n, x in enumerate(m)]
+    return [(x // b, x % b + w[i % d]) for i, x in enumerate(e)], b
 
 
-def decrypt(encrypted, password):
-    b, p = encrypted[2], string_to_int_arr(password)
+def decrypt(q, b, p, w):
     k = len(p)
-    e = [b*encrypted[1][i] + x - p[i % k] %
-         k for i, x in enumerate(encrypted[0])]
+    d = len(w)
+    e = [b*x[0] + x[1] - w[i % d] for i, x in enumerate(q)]
     f = [b*p[i % k] - x for i, x in enumerate(e)]
     g, h = sum_of_products(f, f), sum_of_products(f, e) << 1
     d = g*b
     return ''.join(chr((g*x-f[n]*h)//d) for n, x in enumerate(e))
+
+
+def parse_tuple_int_arr(str):
+    return [tuple(int(y) for y in re.sub("\(|\)", '', x).split(',')) for x in str.replace(' ', '')[
+        1:-1].split('),')]
 
 
 def parse_int_arr(str):
@@ -34,15 +40,44 @@ def parse_int_arr(str):
         1:-1].split(',')]
 
 
-if len(argv) < 2 or (argv[1] not in ['-e', '-d']):
-    print('python3 nspe.py [-e|-d]\n-e encrypt\n-d decrypt')
+def generate_w(length):
+    return [randint(-1024, 1025) for _ in range(0, length)]
+
+
+def generate_p(length):
+    return ''.join(chr(randint(33, 126)) for _ in range(0, length))
+
+
+def check(m, p):
+    k = len(p)
+    for i, x in enumerate(m):
+        if x != p[i % k]:
+            return True
+    return False
+
+
+if len(argv) < 2 or (argv[1] not in ['-e', '-d', '-gp', '-gw']):
+    print('python3 nspe.py [-e|-d|-gw|-gp] [length (if -g)] \n-e encrypt\n-d decrypt\n-gp generating first part of password\n-gw generating second part of password')
 else:
     mode = argv[1]
     if mode == '-e':
-        encrypted = encrypt(
-            input('Podaj wiadomość do zaszyfrowania:\t'), input('Podaj hasło:\t'))
-        print("r = {}\nq = {}\nb = {}".format(
-            encrypted[0], encrypted[1], encrypted[2]))
-    else:
-        print("Wiadomość:\t{}".format(decrypt((parse_int_arr(input('r = ')), parse_int_arr(
-            input('q = ')), int(input('b = '))), input('Podaj hasło:\t'))))
+        m = string_to_int_arr(input('Podaj wiadomość do zaszyfrowania:\t'))
+        p = string_to_int_arr(input('Podaj pierwszą część hasła:\t'))
+        w = parse_int_arr(input('Podaj drugą część hasła:\t'))
+
+        if not check(m, p):
+            print("Hasło i wiadomość muszą mieć przynajmniej jedną różną literę.")
+        else:
+            encrypted = encrypt(m, p, w)
+            print("Zaszyfrowana wiadmość:\t{}\nb = {}".format(
+                encrypted[0], encrypted[1]))
+    elif mode == '-d':
+        q = parse_tuple_int_arr(input('Podaj zaszyfrowaną do wiadomość:\t'))
+        p = string_to_int_arr(input('Podaj pierwszą część hasła:\t'))
+        w = parse_int_arr(input('Podaj drugą część hasła:\t'))
+        b = int(input('b = '))
+        print("Wiadomość:\t{}".format(decrypt(q, b, p, w)))
+    elif mode == '-gw':
+        print(generate_w(int(argv[2])))
+    elif mode == '-gp':
+        print(generate_p(int(argv[2])))
